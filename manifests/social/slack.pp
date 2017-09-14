d48a2ad251652668d9afc2bda1ccdd21045f44cf
Peter Bittner <bittner@comsolit.com>
2017-09-14 16:51:21 +0200

    Add packagecloud APT sources for Slack regularly
    (packagecloud::repo class adds sources to non-existent URLs)

--- manifests/social/slack.pp
+++ manifests/social/slack.pp
@@ -1,23 +1,34 @@
 # slack.pp
 # Install Slack for OS X, Ubuntu, or Windows
-# https://www.slack.com
+# https://www.slack.com, https://packagecloud.io/slacktechnologies
 #
 
 class software::social::slack (
-  $ensure  = $software::params::software_ensure,
+  $ensure = $software::params::software_ensure,
 ) inherits software::params {
 
   validate_string($ensure)
 
   case $::operatingsystem {
     'Ubuntu': {
-      # from https://packagecloud.io/slacktechnologies/slack/install#puppet
-      include '::apt'
-      include packagecloud
+      $apt_source_ensure = $ensure ? {
+        'installed' => present,
+        'latest'    => present,
+        default     => $ensure,
+      }
 
-      packagecloud::repo { 'slacktechnologies/slack':
-        type => 'deb',
-      } -> Class['apt::update'] ->
+      include '::apt'
+      apt::source { 'slack':
+        ensure   => $apt_source_ensure,
+        comment  => 'Slack APT sources for any Debian-based distribution, including Ubuntu',
+        location => 'https://packagecloud.io/slacktechnologies/slack/debian',
+        release  => 'jessie',
+        repos    => 'main',
+        key      => {
+          'id'     => 'C6ABDCF64DB9A0B2',
+          'source' => 'https://packagecloud.io/slacktechnologies/slack/gpgkey',
+        },
+      }
 
       package { 'slack-desktop':
         ensure => $ensure,
